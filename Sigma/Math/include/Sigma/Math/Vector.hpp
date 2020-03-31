49e6ba2e9100856f9384e1f4a9871cc92e6fa4f5
Arman Imani <armanimani87@gmail.com>
2020-03-31 20:28:29 +0200

    Implemented the basic accessors and some operators for the vector class. Added unit test for these operators

--- Sigma/Math/include/Sigma/Math/Vector.hpp
+++ Sigma/Math/include/Sigma/Math/Vector.hpp
@@ -1,11 +1,200 @@
 #pragma once
-#include <concepts>
+
+#include <array>
+#include <numeric>
+
 
 namespace sigma::math
 {
-	template<typename T, std::integral D>
+	template <typename T, std::size_t D>
 	class Vector
 	{
+		static_assert(D > 0);
 	public:
+		using value_type = T;
+		constexpr static std::size_t k_dimension = D;
+
+		constexpr Vector() = default;
+		constexpr explicit Vector(T fill_value);
+		constexpr Vector(T x, T y);
+		constexpr Vector(T x, T y, T z);
+		constexpr Vector(T x, T y, T z, T w);
+
+		constexpr Vector(const Vector&) = default;
+		constexpr Vector(Vector&& other) noexcept = default;
+		constexpr Vector& operator=(const Vector& other) = default;
+		constexpr Vector& operator=(Vector&& other) noexcept = default;
+		~Vector() = default;
+
+		[[nodiscard]] constexpr T& operator[](std::size_t index) noexcept;
+		[[nodiscard]] constexpr const T& operator[](std::size_t index) const noexcept;
+
+		[[nodiscard]] constexpr T& x() noexcept;
+		[[nodiscard]] constexpr T& y() noexcept;
+		[[nodiscard]] constexpr T& z() noexcept;
+		[[nodiscard]] constexpr T& w() noexcept;
+		[[nodiscard]] constexpr const T& x() const noexcept;
+		[[nodiscard]] constexpr const T& y() const noexcept;
+		[[nodiscard]] constexpr const T& z() const noexcept;
+		[[nodiscard]] constexpr const T& w() const noexcept;
+		[[nodiscard]] constexpr T& r() noexcept;
+		[[nodiscard]] constexpr T& g() noexcept;
+		[[nodiscard]] constexpr T& b() noexcept;
+		[[nodiscard]] constexpr T& a() noexcept;
+		[[nodiscard]] constexpr const T& r() const noexcept;
+		[[nodiscard]] constexpr const T& g() const noexcept;
+		[[nodiscard]] constexpr const T& b() const noexcept;
+		[[nodiscard]] constexpr const T& a() const noexcept;
+	private:
+		std::array<T, D> m_data{};
 	};
+
+	
+	template <typename T, std::size_t D>
+	constexpr Vector<T, D>::Vector(T fill_value)
+	{
+		std::fill(m_data.begin(), m_data.end(), fill_value);
+	}
+
+	template <typename T, std::size_t D>
+	constexpr Vector<T, D>::Vector(const T x, const T y)
+		: m_data{x, y}
+	{
+		static_assert(D >= 2);
+	}
+
+	template <typename T, std::size_t D>
+	constexpr Vector<T, D>::Vector(const T x, const T y, const T z)
+		: m_data{ x, y, z }
+	{
+		static_assert(D >= 3);
+	}
+
+	template <typename T, std::size_t D>
+	constexpr Vector<T, D>::Vector(const T x, const T y, const T z, const T w)
+		: m_data{ x, y, z, w }
+	{
+		static_assert(D >= 4);
+	}
+
+
+	template <typename T, std::size_t D>
+	constexpr T& Vector<T, D>::operator[](const std::size_t index) noexcept
+	{
+		return m_data[index];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr const T& Vector<T, D>::operator[](const std::size_t index) const noexcept
+	{
+		return m_data[index];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr T& Vector<T, D>::x() noexcept
+	{
+		return m_data[0];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr T& Vector<T, D>::y() noexcept
+	{
+		static_assert(D >= 2);
+		return m_data[1];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr T& Vector<T, D>::z() noexcept
+	{
+		static_assert(D >= 3);
+		return m_data[2];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr T& Vector<T, D>::w() noexcept
+	{
+		static_assert(D >= 4);
+		return m_data[3];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr const T& Vector<T, D>::x() const noexcept
+	{
+		return m_data[0];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr const T& Vector<T, D>::y() const noexcept
+	{
+		static_assert(D >= 2);
+		return m_data[1];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr const T& Vector<T, D>::z() const noexcept
+	{
+		static_assert(D >= 3);
+		return m_data[2];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr const T& Vector<T, D>::w() const noexcept
+	{
+		static_assert(D >= 4);
+		return m_data[3];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr T& Vector<T, D>::r() noexcept
+	{
+		return m_data[0];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr T& Vector<T, D>::g() noexcept
+	{
+		static_assert(D >= 2);
+		return m_data[1];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr T& Vector<T, D>::b() noexcept
+	{
+		static_assert(D >= 3);
+		return m_data[2];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr T& Vector<T, D>::a() noexcept
+	{
+		static_assert(D >= 4);
+		return m_data[3];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr const T& Vector<T, D>::r() const noexcept
+	{
+		return m_data[0];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr const T& Vector<T, D>::g() const noexcept
+	{
+		static_assert(D >= 2);
+		return m_data[1];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr const T& Vector<T, D>::b() const noexcept
+	{
+		static_assert(D >= 3);
+		return m_data[2];
+	}
+
+	template <typename T, std::size_t D>
+	constexpr const T& Vector<T, D>::a() const noexcept
+	{
+		static_assert(D >= 4);
+		return m_data[3];
+	}
 }
--- Sigma/Math/test/test_vector.cpp
+++ Sigma/Math/test/test_vector.cpp
@@ -1,6 +1,166 @@
 #include <gtest/gtest.h>
 
-TEST(vector, equal)
+#include <Sigma/Math/Vector.hpp>
+
+using namespace sigma::math;
+
+TEST(Vector, Constructor_Default)
 {
-	EXPECT_EQ(1, 1);
+	constexpr Vector<int, 2> vec{};
+
+	EXPECT_EQ(vec[0], 0);
+	EXPECT_EQ(vec[1], 0);
+}
+
+TEST(Vector, Constructor_Fill)
+{
+	constexpr Vector<int, 2> vec{10};
+
+	EXPECT_EQ(vec[0], 10);
+	EXPECT_EQ(vec[1], 10);
+}
+
+TEST(Vector, Constructor_TwoValue)
+{
+	constexpr Vector<int, 2> vec{ 10, 11 };
+
+	EXPECT_EQ(vec[0], 10);
+	EXPECT_EQ(vec[1], 11);
+}
+
+TEST(Vector, Constructor_ThreeValue)
+{
+	constexpr Vector<int, 3> vec{ 10, 11, 12 };
+
+	EXPECT_EQ(vec[0], 10);
+	EXPECT_EQ(vec[1], 11);
+	EXPECT_EQ(vec[2], 12);
+}
+
+TEST(Vector, Constructor_FourValue)
+{
+	constexpr Vector<int, 4> vec{ 10, 11, 12, 14 };
+
+	EXPECT_EQ(vec[0], 10);
+	EXPECT_EQ(vec[1], 11);
+	EXPECT_EQ(vec[2], 12);
+	EXPECT_EQ(vec[3], 14);
+}
+
+TEST(Vector, OperatorBracket_Const)
+{
+	constexpr Vector<int, 2> vec{ 10, 11 };
+	
+	EXPECT_EQ(vec[0], 10);
+	EXPECT_EQ(vec[1], 11);
+}
+
+TEST(Vector, OperatorBracket_NonConst)
+{
+	Vector<int, 2> vec{ 10, 11 };
+
+	EXPECT_EQ(vec[0], 10);
+	EXPECT_EQ(vec[1], 11);
+
+	vec[0] = 2;
+	vec[1] = 5;
+	
+	EXPECT_EQ(vec[0], 2);
+	EXPECT_EQ(vec[1], 5);
+}
+
+TEST(Vector, CopyConstructor)
+{
+	constexpr Vector<int, 2> source{ 10, 11 };
+	constexpr auto target = source;
+
+	EXPECT_EQ(target[0], 10);
+	EXPECT_EQ(target[1], 11);
+}
+
+TEST(Vector, CopyAssignment)
+{
+	constexpr Vector<int, 2> source{ 10, 11 };
+	constexpr auto target = source;
+
+	EXPECT_EQ(target[0], 10);
+	EXPECT_EQ(target[1], 11);
+}
+
+TEST(Vector, MoveConstructor)
+{
+	Vector<int, 2> source{ 10, 11 };
+	auto target{ std::move(source) };
+
+	EXPECT_EQ(target[0], 10);
+	EXPECT_EQ(target[1], 11);
+}
+
+TEST(Vector, MoveAssignment)
+{
+	Vector<int, 2> source{ 10, 11 };
+	auto target = std::move(source);
+
+	EXPECT_EQ(target[0], 10);
+	EXPECT_EQ(target[1], 11);
+}
+
+TEST(Vector, AccessorsXYZW_Const)
+{
+	constexpr Vector<int, 4> source{ 10, 11, 12, 13 };
+
+	EXPECT_EQ(source.x(), 10);
+	EXPECT_EQ(source.y(), 11);
+	EXPECT_EQ(source.z(), 12);
+	EXPECT_EQ(source.w(), 13);
+}
+
+TEST(Vector, AccessorsXYZW_NonConst)
+{
+	Vector<int, 4> source{ 10, 11, 12, 13 };
+
+	EXPECT_EQ(source.x(), 10);
+	EXPECT_EQ(source.y(), 11);
+	EXPECT_EQ(source.z(), 12);
+	EXPECT_EQ(source.w(), 13);
+
+	source.x() = 20;
+	source.y() = 21;
+	source.z() = 22;
+	source.w() = 23;
+
+	EXPECT_EQ(source.x(), 20);
+	EXPECT_EQ(source.y(), 21);
+	EXPECT_EQ(source.z(), 22);
+	EXPECT_EQ(source.w(), 23);
+}
+
+TEST(Vector, AccessorsRGBA_CONST)
+{
+	constexpr Vector<int, 4> source{ 10, 11, 12, 13 };
+
+	EXPECT_EQ(source.r(), 10);
+	EXPECT_EQ(source.g(), 11);
+	EXPECT_EQ(source.b(), 12);
+	EXPECT_EQ(source.a(), 13);
+}
+
+TEST(Vector, NonConstAccessorsRGBA_NonConst)
+{
+	Vector<int, 4> source{ 10, 11, 12, 13 };
+
+	EXPECT_EQ(source.r(), 10);
+	EXPECT_EQ(source.g(), 11);
+	EXPECT_EQ(source.b(), 12);
+	EXPECT_EQ(source.a(), 13);
+
+	source.r() = 20;
+	source.g() = 21;
+	source.b() = 22;
+	source.a() = 23;
+
+	EXPECT_EQ(source.r(), 20);
+	EXPECT_EQ(source.g(), 21);
+	EXPECT_EQ(source.b(), 22);
+	EXPECT_EQ(source.a(), 23);
 }
