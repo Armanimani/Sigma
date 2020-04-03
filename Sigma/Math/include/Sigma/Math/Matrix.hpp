bd7a6cf6969bf248a39cea2ca6a7767f97e265a9
Arman Imani <armanimani87@gmail.com>
2020-04-04 00:50:19 +0200

    Added operator at and multiplies for the matrix. Changed the matrix to be the column-wise

--- Sigma/Math/include/Sigma/Math/Matrix.hpp
+++ Sigma/Math/include/Sigma/Math/Matrix.hpp
@@ -17,9 +17,16 @@ namespace sigma::math
 		friend class Matrix;
 
 		constexpr Matrix() = default;
+		constexpr explicit Matrix(T diagonal_value);
 
 		[[nodiscard]] constexpr T& operator()(std::size_t row_index, std::size_t column_index) noexcept;
 		[[nodiscard]] constexpr const T& operator()(std::size_t row_index, std::size_t column_index) const noexcept;
+
+		template <typename T1, std::size_t P, std::enable_if_t<std::is_same_v<T1, T>>* = nullptr>
+		[[nodiscard]] Matrix<T, M, P> operator*(const Matrix<T1, N, P>& other) const;
+
+		[[nodiscard]] constexpr T& at(std::size_t row_index, std::size_t column_index) noexcept;
+		[[nodiscard]] constexpr const T& at(std::size_t row_index, std::size_t column_index) const noexcept;
 	private:
 		std::array<T, M* N> m_data{};
 		
@@ -27,9 +34,39 @@ namespace sigma::math
 	};
 
 	template <typename T, std::size_t M, std::size_t N>
+	constexpr Matrix<T, M, N>::Matrix(const T diagonal_value)
+	{
+		static_assert(M == N);
+		for (std::size_t i = 0; i != N; ++i)
+		{
+			at(i, i) = diagonal_value;
+		}
+	}
+
+	template <typename T, std::size_t M, std::size_t N>
+	template <typename T1, std::size_t P, std::enable_if_t<std::is_same_v<T1, T>>*>
+	Matrix<T, M, P> Matrix<T, M, N>::operator*(const Matrix<T1, N, P>& other) const
+	{
+		Matrix<T, M, P> result {};
+		for (std::size_t column_index = 0; column_index != P; ++column_index)
+		{
+			for (std::size_t row_index = 0; row_index != M; ++row_index)
+			{
+				T value{};
+				for (std::size_t index = 0; index != N; ++index)
+				{
+					value += at(row_index, index) * other.at(index, column_index);
+				}
+				result.at(row_index, column_index) = value;
+			}
+		}
+		return result;
+	}
+	
+	template <typename T, std::size_t M, std::size_t N>
 	constexpr T& Matrix<T, M, N>::operator()(const std::size_t row_index, const std::size_t column_index) noexcept
 	{
-		assert(row_index < M&& column_index < N);
+		assert(row_index < M && column_index < N);
 		auto index = get_data_index(row_index, column_index);
 		return m_data[index];
 	}
@@ -37,15 +74,27 @@ namespace sigma::math
 	template <typename T, std::size_t M, std::size_t N>
 	constexpr const T& Matrix<T, M, N>::operator()(const std::size_t row_index, const std::size_t column_index) const noexcept
 	{
-		assert(row_index < M&& column_index < N);
+		assert(row_index < M && column_index < N);
 		const auto index = get_data_index(row_index, column_index);
 		return m_data[index];
 	}
 
 	template <typename T, std::size_t M, std::size_t N>
+	constexpr T& Matrix<T, M, N>::at(const std::size_t row_index, const std::size_t column_index) noexcept
+	{
+		return operator()(row_index, column_index);
+	}
+
+	template <typename T, std::size_t M, std::size_t N>
+	constexpr const T& Matrix<T, M, N>::at(const std::size_t row_index, const std::size_t column_index) const noexcept
+	{
+		return operator()(row_index, column_index);
+	}
+
+	template <typename T, std::size_t M, std::size_t N>
 	constexpr std::size_t Matrix<T, M, N>::get_data_index(const std::size_t row_index, const std::size_t column_index) noexcept
 	{
-		assert(row_index < M&& column_index < N);
-		return row_index * N + column_index;
+		assert(row_index < M && column_index < N);
+		return column_index * M + row_index;
 	}
 }
--- Sigma/Math/test/test_Matrix.cpp
+++ Sigma/Math/test/test_Matrix.cpp
@@ -4,7 +4,33 @@
 
 using namespace sigma::math;
 
-TEST(Matrix, DefaultConstructor)
+constexpr auto get_lhs_for_multiplies()
+{
+	Matrix<int, 2, 3> mat{};
+	mat(0, 0) = 1;
+	mat(0, 1) = 2;
+	mat(0, 2) = 3;
+	mat(1, 0) = 4;
+	mat(1, 1) = 5;
+	mat(1, 2) = 6;
+
+	return mat;
+}
+
+constexpr auto get_rhs_for_multiplies()
+{
+	Matrix<int, 3, 2> mat{};
+	mat(0, 0) = 11;
+	mat(0, 1) = 12;
+	mat(1, 0) = 13;
+	mat(1, 1) = 14;
+	mat(2, 0) = 15;
+	mat(2, 1) = 16;
+
+	return mat;
+}
+
+TEST(Matrix, ConstructorDefault)
 {
 	constexpr Matrix<int, 2, 2> mat{};
 
@@ -12,6 +38,16 @@ TEST(Matrix, DefaultConstructor)
 	EXPECT_EQ(mat(0, 1), 0);
 	EXPECT_EQ(mat(1, 0), 0);
 	EXPECT_EQ(mat(1, 1), 0);
+}
+
+TEST(Matrix, ConstructorDiagonal)
+{
+	constexpr Matrix<int, 2, 2> mat{10};
+
+	EXPECT_EQ(mat(0, 0), 10);
+	EXPECT_EQ(mat(0, 1), 0);
+	EXPECT_EQ(mat(1, 0), 0);
+	EXPECT_EQ(mat(1, 1), 10);
 }
 
 TEST(Matrix, OperatorCall_Const)
@@ -42,4 +78,50 @@ TEST(Matrix, OperatorCall_NonConst)
 	EXPECT_EQ(mat(0, 1), 2);
 	EXPECT_EQ(mat(1, 0), 3);
 	EXPECT_EQ(mat(1, 1), 4);
+}
+
+TEST(Matrix, OperatorAt_Const)
+{
+	constexpr Matrix<int, 2, 2> mat{};
+
+	EXPECT_EQ(mat.at(0, 0), 0);
+	EXPECT_EQ(mat.at(0, 1), 0);
+	EXPECT_EQ(mat.at(1, 0), 0);
+	EXPECT_EQ(mat.at(1, 1), 0);
+}
+
+TEST(Matrix, OperatorAt_NonConst)
+{
+	Matrix<int, 2, 2> mat{};
+
+	EXPECT_EQ(mat.at(0, 0), 0);
+	EXPECT_EQ(mat.at(0, 1), 0);
+	EXPECT_EQ(mat.at(1, 0), 0);
+	EXPECT_EQ(mat(1, 1), 0);
+
+	mat.at(0, 0) = 1;
+	mat.at(0, 1) = 2;
+	mat.at(1, 0) = 3;
+	mat.at(1, 1) = 4;
+
+	EXPECT_EQ(mat.at(0, 0), 1);
+	EXPECT_EQ(mat.at(0, 1), 2);
+	EXPECT_EQ(mat.at(1, 0), 3);
+	EXPECT_EQ(mat.at(1, 1), 4);
+}
+
+TEST(Matrix, OperatorMultiplies)
+{
+	const auto lhs = get_lhs_for_multiplies();
+	const auto rhs = get_rhs_for_multiplies();
+
+	auto result = lhs * rhs;
+
+	EXPECT_EQ(result.at(0, 0), 1 * 11 + 2 * 13 + 3 * 15);
+	EXPECT_EQ(result.at(0, 1), 1 * 12 + 2 * 14 + 3 * 16);
+	EXPECT_EQ(result.at(1, 0), 4 * 11 + 5 * 13 + 6 * 15);
+	EXPECT_EQ(result.at(1, 1), 4 * 12 + 5 * 14 + 6 * 16);
+
+	EXPECT_EQ(result.k_row_count, 2);
+	EXPECT_EQ(result.k_column_count, 2);
 }
